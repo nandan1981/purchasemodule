bb540b29ce00b69b706169cd94bf569d1f3cd17a
Nandan1981 <Nandan1981>
2018-12-05 12:41:42 +0530

    CRUD functions to repository class

--- src/main/java/com/rent/model/ProductDetailsDAO.java
+++ src/main/java/com/rent/model/ProductDetailsDAO.java
@@ -1,4 +1,88 @@
 package com.rent.model;
 
-public class ProductDetailsDAO {
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public class ProductDetailsDAO implements CrudRepository {
+    /**
+     * Returns a string representation of the object. In general, the
+     * {@code toString} method returns a string that
+     * "textually represents" this object. The result should
+     * be a concise but informative representation that is easy for a
+     * person to read.
+     * It is recommended that all subclasses override this method.
+     * <p>
+     * The {@code toString} method for class {@code Object}
+     * returns a string consisting of the name of the class of which the
+     * object is an instance, the at-sign character `{@code @}', and
+     * the unsigned hexadecimal representation of the hash code of the
+     * object. In other words, this method returns a string equal to the
+     * value of:
+     * <blockquote>
+     * <pre>
+     * getClass().getName() + '@' + Integer.toHexString(hashCode())
+     * </pre></blockquote>
+     *
+     * @return a string representation of the object.
+     */
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public Object save(Object o) {
+        return null;
+    }
+
+    @Override
+    public Iterable saveAll(Iterable iterable) {
+        return null;
+    }
+
+    @Override
+    public Optional findById(Object o) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterable findAll() {
+        return null;
+    }
+
+    @Override
+    public Iterable findAllById(Iterable iterable) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Object o) {
+
+    }
+
+    @Override
+    public void delete(Object o) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable iterable) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
 }
